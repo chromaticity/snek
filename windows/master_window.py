e855b19ad36eaffc387afd8cc4b1d5467540c7d8
Brandon Antillon <brandonantil457@gmail.com>
2020-08-31 21:15:38 -0700

    Added black bg

--- windows/master_window.py
+++ windows/master_window.py
@@ -9,5 +9,11 @@ class MasterWindow:
         self.root_window = root_window
         root_window.geometry("600x600")
         root_window.title("snek")
+        root_window.configure(bg="black")
+
+        # pack actually places the label on the screen.
         self.label = Label(root_window, text="Snake game goes here...")
         self.label.pack()
+
+    def init_game(self) -> None:
+        pass
